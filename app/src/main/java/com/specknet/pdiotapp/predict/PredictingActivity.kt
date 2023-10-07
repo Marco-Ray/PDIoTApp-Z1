a0079afce681f80e0701456f3518783e01e9327f
Marco-Ray <wangzehuan2017@sina.com>
2023-10-07 21:50:37 +0100

    initial commit

---
+++ app/src/main/java/com/specknet/pdiotapp/predict/PredictingActivity.kt
@@ -0,0 +1,15 @@
+package com.specknet.pdiotapp.predict
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.specknet.pdiotapp.R
+
+
+class PredictingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_predict)
+
+    }
+}
